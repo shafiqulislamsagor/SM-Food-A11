22e8f6c0c1454c550fccc96addf66f0c0db7ff58
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-05-13 01:43:33 +0600

    deploy link

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://sm-food.web.app'],
     credentials: true,
     optionsSuccessStatus: 200
 }))
